2f944f9f44c016156c5a6507f8e0520d93272b35
Arc Angelo Ibalio <gentlyawesome@gmail.com>
2022-01-19 01:20:00 +0800

    fix window

--- docs/assets/scripts/index.js
+++ docs/assets/scripts/index.js
@@ -1,4 +1,4 @@
-;(function () {
+(function () {
   if (typeof window.ethereum !== "undefined" || typeof window.web3 !== "undefined") {
     const provider = window["ethereum"] || window.web3.currentProvider
     console.log({ provider })
